68bff05c6306032f67a99599c532edbc377e7387
d4rk0wl <will.pfanner@gmail.com>
2023-11-27 12:45:35 -0800

    2nd wave fixes (#3)
    GitHub appears to be working again
    
    * Built router, added volume and fixed some bugs
    
    * Forgot to add home link from main icon
    
    ---------
    
    Co-authored-by: Will Pfanner <wpfanner@appliedsense.tech>

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -32,6 +32,7 @@ export function Settings({setdarkMode, darkMode}:Props):JSX.Element{
   const [wpm, setWpm] = useState<number>(20)
   const [farnsworth, setFarnsworth] = useState<number>(0)
   const [tone, setTone] = useState<number>(600)
+  const [ volume, setVolume ] = useState<number>(1)
   const [ hints, setHints ] = useState<boolean>(false)
   const [ autoplay, setAutoPlay ] = useState<boolean>(false)
   const [ cheatMode, setCheatMode ] = useState<boolean>(false) 
@@ -40,6 +41,7 @@ export function Settings({setdarkMode, darkMode}:Props):JSX.Element{
     setWpm(Number(window.localStorage.getItem('wpm')))
     setFarnsworth(Number(window.localStorage.getItem('farnsworth')) * 2)
     setTone(Number(window.localStorage.getItem('tone')))
+    setVolume(Number(window.localStorage.getItem('volume')) * 100)
     setHints((window.localStorage.getItem('hints')) == 'true') //Janky code to convert string to boolean
     setAutoPlay((window.localStorage.getItem('auto_play')) == 'true')
     setCheatMode((window.localStorage.getItem('cheat_mode')) == 'true')
@@ -52,23 +54,28 @@ export function Settings({setdarkMode, darkMode}:Props):JSX.Element{
     window.localStorage.setItem('hints', String(hints))
     window.localStorage.setItem('auto_play', String(autoplay))
     window.localStorage.setItem('cheat_mode', String(cheatMode))
+    window.localStorage.setItem('volume', String(volume * 0.01))
   }
   return(
     <>
       <div className="settings">
         <div className='settings-content'>
-          <Tooltip positioning={'before'} content="Adjust the Words Per Minute (WPM) of the morse code being played. This is the speed in which characters are transmitted" relationship='description'>
+          <Tooltip positioning={'below'} content="Adjust the Words Per Minute (WPM) of the morse code being played. This is the speed in which characters are transmitted" relationship='description'>
             <Label htmlFor='wpm'>Words per Minute: {wpm}wpm</Label>
           </Tooltip>
           <Slider value={wpm} step={5} min={15} max={40} size={'medium'} onChange={(e) => setWpm(parseInt(e.target.value))} />
-          <Tooltip positioning={'before'} content="Adjust the Farnsworth delay between characters. This delay time is interjected between each of the letters or numbers of a transmission" relationship='description'>
+          <Tooltip positioning={'below'} content="Adjust the Farnsworth delay between characters. This delay time is interjected between each of the letters or numbers of a transmission" relationship='description'>
             <Label htmlFor='farnsworth'>Farnsworth Delay: {farnsworth / 2}s</Label>
           </Tooltip>
           <Slider value={farnsworth} min={0} max={20} size={'medium'} onChange={(e) => setFarnsworth(parseInt(e.target.value))} />
-          <Tooltip positioning={'before'} content="Adjust the tone (Hz) in which the oscilliator plays. Some people prefer a lower or higher sine wave tone." relationship='description'>
+          <Tooltip positioning={'below'} content="Adjust the tone (Hz) in which the oscillator plays. Some people prefer a lower or higher sine wave tone." relationship='description'>
             <Label htmlFor='tone'>Tone: {tone}hz</Label>
           </Tooltip>
           <Slider value={tone} step={50} min={400} max={1000} size={'medium'} onChange={(e) => setTone(parseInt(e.target.value))} />
+          <Tooltip positioning={'below'} content="Adjust the playback volume of the CW oscillator" relationship='description'>
+            <Label htmlFor='volume'>Volume: {volume}</Label>
+          </Tooltip>
+          <Slider value={volume} step={1} min={0} max={100} size={'medium'} onChange={(e) => setVolume(parseInt(e.target.value))} />
           <div className="switch-grid">
             <div className="settings-grid-box">
               <Switch className={styles.switch} label={`Dark Theme`} onChange={() => setdarkMode()} checked={darkMode} />
@@ -105,5 +112,6 @@ export function setDefault():void{
     window.localStorage.setItem('saved_settings', 'true')
     window.localStorage.setItem('auto_play', 'true')
     window.localStorage.setItem('cheat_mode', 'false')
+    window.localStorage.setItem('volume', '0.80')
   }
 }
