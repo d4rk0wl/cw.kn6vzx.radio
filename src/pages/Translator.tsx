dbc347d60f1304c380fe768474ecb3a8f19ddecf
d4rk0wl <will.pfanner@gmail.com>
2023-11-27 15:17:18 -0800

    Forgot the translator

--- src/pages/Translator.tsx
+++ src/pages/Translator.tsx
@@ -16,6 +16,9 @@ export default function Translator(){
   const [ history, setHistory ] = useState<{historicalWord: string, code: string[]}[]>([])
 
   const submit = () => {
+    if(!word){
+      return
+    }
     Oscillator(word)
     setHistory(history => [...history, {historicalWord: word, code: GenerateMorseSync(word)}])
   }
@@ -25,7 +28,7 @@ export default function Translator(){
       <div className="content-wrapper">
         <div className="grid-box grid-col-span-2">
           <Label className='input-form' size={'large'} htmlFor='input'>Input Words or Sentence to Translate: </Label>
-          <MainInput change={(e) => setWord(e.target.value)} value={word} placeholder='Enter Word or Phrase' submit={() => submit()} />
+          <MainInput change={(e) => setWord(e.target.value)} value={word} placeholder='Enter Word or Phrase' submit={() => submit()} disable={false} />
         </div>
         <div className="grid-box grid-col-span-2">
           <History historyArray={history} />
